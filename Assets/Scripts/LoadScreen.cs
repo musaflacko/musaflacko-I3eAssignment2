868d05e2bebc1b644351c526c4e18bfa90a420de
weifeng-l <s10208179@connect.np.edu.sg>
2021-07-12 15:50:01 +0800

    BackGround Audio

--- Assets/Scripts/LoadScreen.cs
+++ Assets/Scripts/LoadScreen.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class LoadScreen : MonoBehaviour
 {
-
-    public GameObject LoadingScreen;
     // Start is called before the first frame update
     void Start()
     {
