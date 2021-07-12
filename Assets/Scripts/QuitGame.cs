c0f6d1d16d6df715d7d21dca41a1e34d645a12a6
weifeng-l <s10208179@connect.np.edu.sg>
2021-07-12 17:05:07 +0800

    Main Menu setup

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScreen : MonoBehaviour
+public class QuitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void ExitGame()
     {
-        Debug.Log("ExitGame");
+        Debug.Log("QuitGame");
         Application.Quit();
     }
 }
