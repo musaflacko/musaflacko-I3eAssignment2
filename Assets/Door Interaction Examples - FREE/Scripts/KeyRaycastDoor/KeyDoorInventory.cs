2716eb4a61c8f867cd537c4cbabee553420ed2e0
weifeng-l <s10208179@connect.np.edu.sg>
2021-07-17 23:59:59 +0800

    testing push and pull

--- Assets/Door Interaction Examples - FREE/Scripts/KeyRaycastDoor/KeyDoorInventory.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class KeyDoorInventory : MonoBehaviour
-{
-    public bool hasRedKey = false;
-    public bool hasBlueKey = false;
-    public bool hasGreenKey = false;
-}
