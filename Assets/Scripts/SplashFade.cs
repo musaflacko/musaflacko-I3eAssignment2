375fea4eef5fbfa62b11c232e3442a3eaa163401
Musa Flacko <s10194189@connect.np.su.sg>
2021-07-17 18:33:36 +0800

    Splash Screen

---
+++ Assets/Scripts/SplashFade.cs
@@ -0,0 +1,33 @@
+﻿/*Developed and provided by SpeedTutor - www.speed-tutor.com*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashFade : MonoBehaviour
+{
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
